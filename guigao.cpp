8b5bb3b70d12c274ba58ebe1913268b47eb388c0
Barella1x <tnitezinho123@gmail.com>
2024-05-13 23:13:50 -0300

    Add files via upload

---
+++ guigao.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char resposta;
+
+    do {
+        cout << "guigo e gay?" << endl;
+        cout << "Digite 's' para sim ou 'n' para nao" << endl;
+        cout << "RESPOSTA: ";
+        cin >> resposta;
+        cout<<endl;
+    } while (resposta != 's');
+      cout<< "ebaaaaa";
+    return 0;
+}
